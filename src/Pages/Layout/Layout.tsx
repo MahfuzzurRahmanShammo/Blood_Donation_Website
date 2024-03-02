1a7996b0bb78e4c452370ae71b2dd0bbe9e03265
Shammo06 <mrshammo2018@gmail.com>
2024-03-02 16:37:54 +0600

    add sprint one part

---
+++ src/Pages/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router";
+import Navbar from "../../Component/Navbar/Navbar";
+import Footer from "../../Component/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/main.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./Pages/About/About";
+import Mission from "./Pages/Mission";
+import Layout from "./Pages/Layout/Layout";
+import Home from "./Pages/Home/Home";
+import DonorRegistration from "./Pages/DonorRegistration/DonorRegistration";
+import DonorSearch from "./Pages/DonorSearch/DonorSearch";
+import Login from "./Pages/Login/Login";
+import Registration from "./Pages/Registration/Registration";
+import AuthProvider from "./providers/AuthProvider";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/mission",
+        element: <Mission></Mission>,
+      },
+      {
+        path: "/donorRegistration",
+        element: <DonorRegistration></DonorRegistration>
+      },
+      {
+        path: '/donorSearch',
+        element: <DonorSearch></DonorSearch>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Registration></Registration>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
